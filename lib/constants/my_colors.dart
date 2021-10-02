448096709df992264158f1dbeb9feb5db041d0bb
AbuElmagd <mohamed.aboelmagd2021@gmail.com>
2021-10-02 21:01:40 +0200

    - add new color(black)

--- lib/constants/my_colors.dart
+++ lib/constants/my_colors.dart
@@ -5,4 +5,5 @@ class MyColors {
   static const myNavyBlue = Color(0xFF233657);
   static const myGrey = Color(0xFFA5AEBC);
   static const myRed = Color(0xFF800000);
+  static const myBlack = Color(0xFF000000);
 }
